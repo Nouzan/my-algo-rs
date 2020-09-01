d03bec3efb78a76342d22a262679e3ecedd67ef7
Nouzan <clobraindie@outlook.com>
2020-09-01 10:04:06 +0800

    add reverse print example

---
+++ examples/single_head_linked_list_reverse_print.rs
@@ -0,0 +1,29 @@
+use my_algo::ch2::linked_list::single_head::LinkedList;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    input: Vec<u64>,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    println!("inputs: {:?}", opt.input);
+
+    let mut list = LinkedList::default();
+    let mut cursor = list.cursor_mut();
+    for v in opt.input.iter() {
+        cursor.insert_after(*v);
+        cursor.move_next();
+    }
+
+    let mut stack = Vec::new();
+    for v in list.iter_mut() {
+        stack.push(*v);
+    }
+    print!("outputs: ");
+    while !stack.is_empty() {
+        print!("{} ", stack.pop().unwrap())
+    }
+    print!("\n")
+}
