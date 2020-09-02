196d561c84eef527a2239281f3001ff86a621eb3
Nouzan <clobraindie@outlook.com>
2020-09-02 09:29:08 +0800

    添加了一些基于带头结点的单链表的小算法 (#16)
    * add `sorted_print` example
    
    * impl `Debug` && add `split_odd` example
    
    * add test && change the structures

---
+++ examples/shll_sorted_print.rs
@@ -0,0 +1,16 @@
+use my_algo::ch2::linked_list::single_head::LinkedList;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    input: Vec<i64>,
+}
+
+// 带头结点的单链表
+// 习题 2.3.9
+fn main() {
+    let opt = Opt::from_args();
+    let mut list = LinkedList::from(opt.input);
+    list.sort();
+    println!("{:?}", list);
+}
---
+++ examples/shll_split_odd.rs
@@ -0,0 +1,18 @@
+use my_algo::ch2::linked_list::single_head::{utils::split_odd, LinkedList};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    input: Vec<u64>,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    println!("inputs: {:?}", opt.input);
+
+    let mut list = LinkedList::from(opt.input);
+    let odd = split_odd(&mut list);
+
+    println!("{:?}", odd);
+    println!("{:?}", list);
+}
